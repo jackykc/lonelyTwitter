7098894dd0e9813eb1e6685ba9c523b5abd40b40
jackykc <jkc1@ualberta.ca>
2017-02-10 02:41:43 -0700

    Started elastic search

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -7,7 +7,13 @@ import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
 /**
  * Created by romansky on 10/20/16.
@@ -20,12 +26,18 @@ public class ElasticsearchTweetController {
 
         @Override
         protected Void doInBackground(NormalTweet... tweets) {
-            //verifySettings();
+            verifySettings();
 
             for (NormalTweet tweet : tweets) {
                 Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
 
                 try {
+                    DocumentResult result = client.execute(index);
+                    if (result.isSucceeded()) {
+                        tweet.setId(result.getId());
+                    } else {
+                        Log.i("Error", "Elasticsearch was not able to add the tweet");
+                    }
                     // where is the client?
                 }
                 catch (Exception e) {
@@ -38,17 +50,46 @@ public class ElasticsearchTweetController {
     }
 
     // TODO we need a function which gets tweets from elastic search
-/*    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
+    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
         @Override
         protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
             verifySettings();
 
             ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
+            String query =
+                    "{ \n\"query\" : {\n" +
+                    "    \"term\" : { \"message\" : \"" + search_parameters[0] +
+                    "\"     }\n " +
+                    "    }\n" +
+                    " } ";
 
+//            String query = "{\n" +
+//                    //"    \"id\": \"myTemplateId\"," +
+//                    "       \"params\": {\n" +
+//                    //"           \"query_string\" : \"" + search_parameters[0] +
+//                    "           \"term\" : { \"message\" : \""+search_parameters[0] +"\" }\n" +
+//                    "\" " +
+//                    "    }\n" +
+//                    "}";
+
+//          String query = "" + search_parameters[0];
+            //query = "";
                 // TODO Build the query
+            System.out.println(query);
+            Search search = new Search.Builder(query)
+                    .addIndex("testing")
+                    .addType("tweet")
+                    .build();
 
             try {
                // TODO get the results of the query
+                SearchResult result = client.execute(search);
+                if(result.isSucceeded()) {
+                    List<NormalTweet> foundTweets = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(foundTweets);
+                } else {
+                    Log.i("Error", "Search query failed to find any tweets that matched");
+                }
             }
             catch (Exception e) {
                 Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
@@ -56,7 +97,7 @@ public class ElasticsearchTweetController {
 
             return tweets;
         }
-    }*/
+    }
 
 
 
